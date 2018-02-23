da624df6c63eec8fe773c3856214ab8c0dc83b7d
Lino Veloso <linoveloso@outlook.com>
2018-02-23 16:43:34 -0300

    Adicionando Projeto de estudos Java com base nas aulas ALURA

---
+++ src/aula4/Hello.java
@@ -0,0 +1,15 @@
+package aula4;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		sayHello();
+	}
+
+	public static void sayHello() {
+System.out.println("");		
+	}
+	
+
+}
