63c6cd41024cf412c79816b2127f65308583d551
Lino Veloso <linoveloso@outlook.com>
2018-02-26 13:21:40 -0300

    Atividade aula 4
    Adicionando Projeto de estudos Java com base nas aulas ALURA

---
+++ src/aula4/Turma.java
@@ -0,0 +1,17 @@
+package aula4;
+
+class Turma {
+	
+}
+class TesteDaTurma{
+	public static void main(String[] args) {
+		int[] notas = new int[10];
+		
+		notas[0] = 9;
+		notas[2] = 5;
+		
+		
+		System.out.println("A nota 1 é: "+notas[0]);
+		System.out.println("A nota 3 é: "+notas[2]);
+	}
+}
--- src/aula4/ex1.java
+++ src/aula4/ex1.java
@@ -1,5 +1,45 @@
 package aula4;
+//Exercício página 50 apostila fj11 da Caelum > https://www.caelum.com.br/download/caelum-java-objetos-fj11.pdf
 
-public class ex1 {
+class ex1 {
+	public static void main(String[] args) {
+		// criando a conta
+		Conta minhaConta = new Conta();
+
+		// colocando alguns dados
+		minhaConta.titular = "Lino";
+		minhaConta.numero = 789;
+		minhaConta.saldo = 2500.0;
+		minhaConta.limite = 1500.0;
+
+		// mostrando o valor do saldo
+		System.out.println("Saldo atual é de: " + minhaConta.saldo + " R$");
+		
+		
+		//saca 200 reais
+		minhaConta.saca(200);
+		
+		//deposita 1000 reais
+		minhaConta.depoista(1000);
+		System.out.println("Após operações bancárias o saldo é: "+minhaConta.saldo);
+		
+	}
+
+}
+
+class Conta {
+	int numero;
+	String titular;
+	double saldo;
+	double limite;
+
+	void saca(double qtd) {
+		double novosaldo = this.saldo - qtd;
+		this.saldo = novosaldo;
+
+	}
+	void depoista(double qtd) {
+		this.saldo += qtd;
+	}
 
 }
