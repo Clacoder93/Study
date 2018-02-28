1c565efd941396e3a53ab7d124c01ae51370bf50
Lino Veloso <linoveloso@outlook.com>
2018-02-28 15:36:41 -0300

    Alteração de Construtores

--- src/aula5/Conta.java
+++ src/aula5/Conta.java
@@ -6,10 +6,15 @@ class Conta {
 	private double saldo;
 	private double limite;
 
-	public Conta (int numero, double limite) {
-		
+	// Construtores
+	public Conta(int numero, double limite) {
+
 	}
-	
+
+	public Conta() {
+
+	}
+
 	public void saca(double qtd) {
 		if (qtd < this.saldo) {
 			this.saldo -= qtd;
@@ -22,11 +27,11 @@ class Conta {
 	public void deposita(double valor) {
 		this.saldo += valor;
 	}
-	
+
 	public double getSaldo() {
 		return this.saldo;
 	}
-	
+
 	public void setLimite(double novoLimite) {
 		this.limite = novoLimite;
 	}
