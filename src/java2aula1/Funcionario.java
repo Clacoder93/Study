765fdf495781dee53d77cc3b708bbbebcfed015f
Lino Veloso <linoveloso@outlook.com>
2018-03-06 17:03:06 -0300

    aula de herança

---
+++ src/java2aula1/Funcionario.java
@@ -0,0 +1,36 @@
+package java2aula1;
+
+class Funcionario {
+	private String nome;
+	private double salario;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public double getBonus() {
+	return this.salario * 0.2;
+	}
+
+}
+
+class testaFuncionario {
+	public static void main(String[] args) {
+		Funcionario lino = new Funcionario();
+		lino.setSalario(500.0);
+		
+		System.out.println(lino.getBonus());
+	}
+}
